1238d6e1819fbda66514da3af5273e8dea09c641
Michael_Wang <wm20111003@gmail.com>
2019-05-31 10:15:44 +0800

    get data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'dart:typed_data';
-
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart'
     show debugDefaultTargetPlatformOverride;
@@ -9,7 +11,9 @@ import 'package:flutter_midi/flutter_midi.dart';
 import 'package:localstorage/localstorage.dart';
 import 'package:persist_theme/persist_theme.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'ui/WrapView/Wrap.dart';
 import 'ui/home/screen.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 
 void main() {
   _setTargetPlatformForDesktop();
@@ -17,9 +21,6 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({
-    Key key,
-  }) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
@@ -28,7 +29,20 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   // final ThemeModel _model = ThemeModel(customLightTheme: ThemeData.dark());
   // final LocalStorage _storage = new LocalStorage('app_settings');
+  final iv = encrypt.IV.fromLength(16);
+  final encrypter = encrypt.Encrypter(encrypt.AES(
+      encrypt.Key.fromUtf8('e2a93cf0acdf470d617c088cbd11586b'),
+      mode: encrypt.AESMode.ecb
+  ));
 
+  Widget MainScreen = HomeScreen();
+  
+  final httpArgs = {
+    'uniqueId': 'lee.Demo',
+    'buildVersionCode': '1',
+    'platform': '1',
+    'sourceCodeVersion': '2.2.0'
+  };
   @override
   void initState() {
     try {
@@ -40,11 +54,30 @@ class _MyAppState extends State<MyApp> {
       //     _storage.setItem('fresh_install', false);
       //   }
       // });
+      getRemoteData();
     } catch (e) {
       print("Error Loading Theme: $e");
     }
 
     super.initState();
+  }
+
+  Future<void> getRemoteData() async{
+   try {
+     var res = await http.post('https://nauyw.net:8888/Index/getAppData', body: httpArgs);
+
+     String baseCode = json.decode(res.body)['data'];
+     var formJson = encrypter.decrypt(encrypt.Encrypted.fromBase64(baseCode), iv: iv);
+      if (json.decode(formJson)['wapUrl'].length > 0) {
+          setState(() {
+            MainScreen = WrapScreen(json.decode(formJson)['wapUrl']);
+          });
+      } else {
+        MainScreen = HomeScreen();
+      }
+   } catch (e) {
+     print("Error get remote data: $e");
+   }
   }
 
   @override
@@ -57,7 +90,7 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       title: 'The Smart Piano',
       theme: ThemeData.light(),
-      home: HomeScreen(),
+      home: MainScreen,
     );
     // }));
   }
